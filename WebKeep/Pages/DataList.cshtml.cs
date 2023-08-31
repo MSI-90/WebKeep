af3d6c4db7e46b36c3c624a299e1623a17bb080b
MSI-90 <104754352+MSI-90@users.noreply.github.com>
2023-08-31 15:36:12 +0700

    Pagination is added on DataList Page

--- WebKeep/Pages/DataList.cshtml.cs
+++ WebKeep/Pages/DataList.cshtml.cs
@@ -2,6 +2,8 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Linq;
+using System.Drawing.Printing;
 using WebKeep.Interfaces;
 using WebKeep.Models;
 
@@ -12,6 +14,8 @@ namespace WebKeep.Pages
     public class DataListModel : PageModel
     {
         private readonly ISavedLinks _savedLinks;
+        private int pageSize = 4;
+        public PagingInfo PageInfo { get; set; }
         public List<DbModel> SavedLinksList { get; set; }
         public int Count { get; set; }
         public SelectListItem[] Categories { get; set; }
@@ -29,20 +33,27 @@ namespace WebKeep.Pages
             }
         }
 
-        public void OnGet()
+        public void OnGet(int? elementPage)
         {
             var taskListResult = string.IsNullOrEmpty(InputSort.Categories)
             ? _savedLinks.GetDataAsync()
             : _savedLinks.GetAllDataFromCategory(InputSort);
 
-            //if (taskListResult != null)
-            //{
             SavedLinksList = taskListResult.Result.ToList();
             Count = SavedLinksList?.Count ?? 0;
 
             for (int i = 0; i < Count; i++)
                 SavedLinksList[i].IndexCount = i+1;
-            //}
+
+            PagingInfo pagInfo = new PagingInfo()
+            {
+                CurrentPage = elementPage ?? 1,
+                ItemsPerPage = pageSize,
+                TotalItems = Count
+            };
+            PageInfo = pagInfo;
+            SavedLinksList = SavedLinksList.OrderBy(x => x.Date).Skip((pagInfo.CurrentPage - 1) * 
+                pageSize).Take(pageSize).ToList();
         }
         
         public class FilterSortModel
